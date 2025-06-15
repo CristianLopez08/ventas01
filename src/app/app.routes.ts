951b3f09b3cba7f87019fdab6fcfd39db42e2cb7
CristianLopez08 <cristianlpz0812@gmail.com>
2025-06-15 11:48:59 -0600

    feat: versión 1 de la aplicación de ventas, incluye imagenes y productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Products } from './pages/products/products';
+import { Productos } from './pages/products/products';
 import { Contact } from './pages/contact/contact';
 import { About } from './pages/about/about';
 import { Home } from './pages/home/home';
@@ -7,7 +7,7 @@ import { Home } from './pages/home/home';
 export const routes: Routes = [
     {path:'', component:Home},
     {path:'nosotros',component:About},
-    {path:'productos', component:Products},
+    {path:'productos', component:Productos},
     {path:'contacto', component:Contact},
     {path:'**', redirectTo:''}
 ];
--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
 
+export interface Product{
+  id: number;
+  nombre:string;
+  descripcion: string;
+  precio: number;
+  imagen: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class Products {
 
-  constructor() { }
+export class Products {
+  
+  private products:Product[] = [
+    {
+      id: 1,
+      nombre: 'Simi fachero',
+      descripcion : 'Simi con skin fachera',
+      precio: 6500,
+      imagen: '/img/simiFacha.jpeg'
+    },
+    {
+      id: 2,
+      nombre: 'Luffy Simi',
+      descripcion: 'Peluche de simi con skin de Luffi OP',
+      precio: 9200,
+      imagen: '/img/simiLuffy.jpeg'
+    },
+    {
+      id: 3,
+      nombre: 'Simi Playero',
+      descripcion: 'Peluche de Simi con skin de playa',
+      precio: 3900,
+      imagen: '/img/simiPlayero.jpeg'
+    },
+   {
+      id: 4,
+      nombre: 'Simi Punketo',
+      descripcion: 'Simi con skin de punketo metalero',
+      precio: 1000,
+      imagen: '/img/simiPunk.jpeg'
+   }
+  ]
+  constructor(){}
+  getProducts():Product[]{
+    return this.products;
+  }
 }
